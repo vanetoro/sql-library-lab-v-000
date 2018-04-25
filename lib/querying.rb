e2ef4186b1f2510e4ac02eb99d6baf82c22da962
Vanessa Toro <vanetoro84@gmail.com>
2018-04-25 21:10:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  ("SELECT authors.name, series.title FROM authors 
+  ("SELECT authors.name, series.title FROM authors
   JOIN series
   ON authors.id = series.author_id;")
 end
