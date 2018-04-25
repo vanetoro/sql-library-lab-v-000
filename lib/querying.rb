1084e6dc2936d2c5e5a0d84f562041d4074fdf7d
Vanessa Toro <vanetoro84@gmail.com>
2018-04-25 21:07:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  ("SELECT characters.species, COUNT (characters.species) FROM characters
+  GROUP BY characters.species
+  ORDER BY characters.species DESC LIMIT 1;")
 end
 
 def select_name_and_series_subgenres_of_authors
