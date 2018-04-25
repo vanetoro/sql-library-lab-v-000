2e9025a5b28598383ab0cbc20125a0dd25abe8cd
Vanessa Toro <vanetoro84@gmail.com>
2018-04-25 21:30:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  ("SELECT series.title FROM series
+  JOIN characters ON characters.series_id = series.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title LIMIT 1")
 end
 
 def select_character_names_and_number_of_books_they_are_in
